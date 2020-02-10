4de11b6e375b8f8ff85b081ab836e27b485662d0
Mohammed Yonus M <mohammedyonusm1@gmail.com>
2020-02-10 22:42:52 +1300

    Testing

--- src/test/java/ChromeTest.java
+++ src/test/java/ChromeTest.java
@@ -26,10 +26,7 @@ public class ChromeTest {
     public void setupTest() {
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized"); // open Browser in maximized mode
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--headless"); // disabling infobars
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox"); // Bypass OS security model
         driver = new ChromeDriver(options);
